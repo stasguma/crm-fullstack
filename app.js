f55156ad98232d76eb2333d4c2b385bbc461e5e0
Alex Gumenyuk <aleks.gumenyuk@gmail.com>
2019-10-06 17:40:10 +0300

    init 2

--- app.js
+++ app.js
@@ -1,46 +1,47 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const passport = require('passport');
-const path = require('path');
-const bodyParser = require('body-parser');
-const authRoutes = require('./routes/auth');
-const analyticsRoutes = require('./routes/analytics');
-const categoryRoutes = require('./routes/category');
-const orderRoutes = require('./routes/order');
-const positionRoutes = require('./routes/position');
-const keys = require('./config/keys');
+const express = require("express");
+const mongoose = require("mongoose");
+const passport = require("passport");
+const path = require("path");
+const bodyParser = require("body-parser");
+const authRoutes = require("./routes/auth");
+const analyticsRoutes = require("./routes/analytics");
+const categoryRoutes = require("./routes/category");
+const orderRoutes = require("./routes/order");
+const positionRoutes = require("./routes/position");
+const keys = require("./config/keys");
 const app = express();
 
-mongoose.connect(keys.mongoURI, {
+mongoose
+  .connect(keys.mongoURI, {
     useNewUrlParser: true,
     useCreateIndex: true
-})
-.then(() => console.log('MongoDB connected.'))
-.catch((error) => console.log(error))
+  })
+  .then(() => console.log("MongoDB connected."))
+  .catch(error => console.log(error));
 
 app.use(passport.initialize());
-require('./middleware/passport')(passport);
+require("./middleware/passport")(passport);
 
-app.use(require('morgan')('dev'));
-app.use('/uploads', express.static('uploads'));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(require("morgan")("dev"));
+app.use("/uploads", express.static("uploads"));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(require('cors')());
+app.use(require("cors")());
 
-app.use('/api/auth', authRoutes);
-app.use('/api/analytics', analyticsRoutes);
-app.use('/api/category', categoryRoutes);
-app.use('/api/order', orderRoutes);
-app.use('/api/position', positionRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/analytics", analyticsRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/order", orderRoutes);
+app.use("/api/position", positionRoutes);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/disc/client'));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/disc/client"));
 
-    app.get('*', (req, res) => {
-        res.sendFile(
-            path.resolve(__dirname, 'client', 'dist', 'client', 'index.html');
-        )
-    })
+  app.get("*", (req, res) => {
+    res.sendFile(
+      path.resolve(__dirname, "client", "dist", "client", "index.html")
+    );
+  });
 }
 
 module.exports = app;
